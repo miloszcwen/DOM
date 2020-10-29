5fcdbb580e65e99a3a31767172c58a0a5b523eff
miloszcwen <73250136+miloszcwen@users.noreply.github.com>
2020-10-29 17:14:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,128 @@
+
+document.querySelectorAll("h1");
+
+// document.querySelector("img").getAttribute("random");
+
+document.getElementById("bobo").onclick = function(){
+document.getElementById("status").innerHTML = "nice job!";
+}
+document.getElementById("status").onclick = function(){
+document.getElementById("status").innerHTML = "";
+}
+
+// var database = [
+// 	{
+// 		username: "andrei",
+// 		password: "supersecret"
+// 	},
+// 	{
+// 		username: "sally",
+// 		password: "cat"
+// 	},
+// 	{
+// 		username: "bob",
+// 		password: "555"
+// 	}
+// ];
+
+// var newsFeed = [
+// 	{
+// 		username: "Milo",
+// 		timeline: "Nice photo"
+// 	},
+// 	{
+// 		username: "Kate",
+// 		timeline: "Look at me"
+// 	},
+// 	{
+// 		username: "Kilan",
+// 		timeline: "JS is cool!"
+// 	},
+// ];
+
+// function isUserValid(user,pass){
+// 	for (var i = 0; i < database.length; i++) {
+// 		if (user===database[i].username
+// 		&&pass===database[i].password) {
+// 				return true;
+// 		} 
+// 	}
+
+// 		return false;
+// }
+
+// function signIn(user,pass){
+// 		if (isUserValid(user,pass)) {
+// 		console.log(newsFeed);
+// 		} else {
+// 		console.log("Wrong username or password");
+// 		}
+// }
+
+// var userNamePrompt = prompt("What's your username?");
+// var passwordPrompt = prompt("What's your password?");
+// signIn(userNamePrompt,passwordPrompt);
+
+
+// var toDoList =[
+// 	"clean room",
+// 	"brush teeth",
+// 	"ecersise",
+// 	"eat healthy"
+// ];
+
+// var a=5;
+
+// // var counter=0;
+// // while (counter<10){
+// // 	console.log(counter);
+// // 	counter++;
+// // }
+
+// // var counter2=0;
+// // do{
+// // 	console.log(counter2);
+// // 	counter2++;
+// // } while (counter2<10);
+
+// // for (var i = 0; i < toDoList.length; i++) {
+// // 	toDoList[i]=toDoList[i]="!";
+// // }
+// // toDoList.forEach(function(it,i,arr){
+// // 	arr[i]=arr[i]+"ss";
+// // })
+
+
+// function age3(age){
+// if (Number(age) < 18) {
+// 	console.log("Sorry, you are too yound to drive this car. Powering off");
+// } else {
+// 	return 2;
+// }
+// }
+
+// var userObj= {
+// 	name:"Milo",
+// 	age:"33",
+// 	hobby:["JS","html","CSS"],
+// 	isMarried:false,
+// 	//shout is a method of user
+// 	shout: function(){
+// 		console.log("aaa xd");
+// 	}
+// };
+// //user.hobby[2]
+// //user.shout()
+// var usersList = [
+// {
+// 	username: "milo",
+// 	password: "cycki"
+// },
+// {
+// 	username: "aga",
+// 	password: "dupad"
+// }
+// ];
+// //users[1].password
+
+
